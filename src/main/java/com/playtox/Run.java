f01f6266bc98cd98a9d4a22c6252e0b571ae5126
VavilovNikita <112397078+VavilovNikita@users.noreply.github.com>
2023-08-25 15:04:53 +0500

    third commit

---
+++ src/main/java/com/playtox/Run.java
@@ -0,0 +1,70 @@
+package com.playtox;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+
+class Run {
+    private void takeLock(Lock lock1, Lock lock2) {
+        boolean firstLockTaken = false;
+        boolean secondLockTaken = false;
+        while (true) {
+            try {
+                firstLockTaken = lock1.tryLock();
+                secondLockTaken = lock2.tryLock();
+            } finally {
+                if (firstLockTaken && secondLockTaken) {
+                    return;
+                }
+
+                if (firstLockTaken) {
+                    lock1.unlock();
+                }
+
+                if (secondLockTaken) {
+                    lock2.unlock();
+                }
+            }
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void makeTransfer(List<Account> accountList, List<Lock> lockList, int id) throws InterruptedException {
+        Random random = new Random();
+        int account1;
+        int account2;
+        do {
+            account1 = random.nextInt(accountList.size());
+            account2 = random.nextInt(accountList.size());
+        } while (account1 == account2);
+        takeLock(lockList.get(account1), lockList.get(account2));
+        try {
+            Account.transfer(accountList.get(account1), accountList.get(account2), random.nextInt(1000), id);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lockList.get(account1).unlock();
+            lockList.get(account2).unlock();
+            try {
+                Thread.sleep(random.nextInt(1000) + 1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+    public void printResult(List<Account> accountList) {
+        int result = 0;
+        for (Account account : accountList) {
+            int balance = account.getBalance();
+            System.out.println("account" + account.getId() + ": " + balance);
+            result += balance;
+        }
+        System.out.println("total " + result);
+    }
+}
